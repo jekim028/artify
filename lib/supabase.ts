74a7fad6689e4929a39f4f647014ed938f81b308
Jenna Kim <jekim028@gmail.com>
2023-08-16 15:43:46 -0700

    set up supabase user management

---
+++ lib/supabase.ts
@@ -0,0 +1,27 @@
+import 'react-native-url-polyfill/auto'
+import * as SecureStore from 'expo-secure-store'
+import { createClient } from '@supabase/supabase-js'
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key)
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value)
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key)
+  },
+}
+
+const supabaseUrl = "https://queshprfaqrnbkmdhoyl.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1ZXNocHJmYXFybmJrbWRob3lsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIxMjQ4MzAsImV4cCI6MjAwNzcwMDgzMH0.2Y8mmIHNhDs3nmR_GDGYKn18zhj0pzhykcvKAYqh61M"
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    storage: ExpoSecureStoreAdapter as any,
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: false,
+  },
+})
